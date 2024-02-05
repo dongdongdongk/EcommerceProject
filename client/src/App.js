5b3e9a8e92ec388e8520cd0574923b7c3b935f80
dongdongdongk <dhk9309@gmail.com>
2024-02-05 12:42:19 +0900

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage/>} />
+        <Route path="/sign-up" element={<SignupPage/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
