d5867236a44a119f0eaa31586ecb9c3f712191b5
dongdongdongk <dhk9309@gmail.com>
2024-02-05 17:15:10 +0900

    SignUp

--- client/src/components/Signup/Signup.jsx
+++ client/src/components/Signup/Signup.jsx
@@ -1,11 +1,23 @@
 import React, { useState } from "react";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import { FiFilePlus } from "react-icons/fi";
 import styles from "../../styles/styles.js";
 import { Link } from "react-router-dom";
 const Signup = () => {
   const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [visible, setVisible] = useState(false);
+  const [avatar, setAvatar] = useState(null);
+
+  const handleSubmit = () => {
+    console.log("제출");
+  };
+
+  const handlerFileInputChange = (e) => {
+    const file = e.target.files(0);
+    setAvatar(file);
+  };
 
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
@@ -16,6 +28,24 @@ const Signup = () => {
         <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
           <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
             <form className="space-y-6">
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  이름
+                </label>
+                <div className="mt-1">
+                  <input
+                    type="text"
+                    name="name"
+                    required
+                    value={name}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+                  />
+                </div>
+              </div>
               <div>
                 <label
                   htmlFor="email"
@@ -65,28 +95,37 @@ const Signup = () => {
                   )}
                 </div>
               </div>
-              <div className={`${styles.noramlFlex} justify-between`}>
-                <div className={`${styles.noramlFlex}`}>
-                  <input
-                    type="checkbox"
-                    name="remember-me"
-                    id="remember-me"
-                    className="h-4 w-4 text-blue-600 focus:ring-blue-50 border-gray-300 rounded"
-                  />
+              <div>
+                <label
+                  htmlFor="avatar"
+                  className="block text-sm font-medium text-gray-700"
+                ></label>
+                <div className="mt-2 flex items-center">
+                  <span className="inline-block h-8 w-8 rounded-none overflow-hidden">
+                    {avatar ? (
+                      <img
+                        src={URL.createObjectURL(avatar)}
+                        alt="avatar"
+                        className="h-full w-full object-cover rounded-full"
+                      />
+                    ) : (
+                      <FiFilePlus className="h-8 w-8"/>
+                    )}
+                  </span>
                   <label
-                    htmlFor="remember"
-                    className="ml-2 block text-sm text-gray-900"
+                    htmlFor="file-input"
+                    className="ml-5 flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
                   >
-                    Remember me
+                    <span>Upload a file</span>
+                    <input
+                      type="file"
+                      name="avatar"
+                      id="file-input"
+                      accept=".jpg,.jpeg,.png"
+                      onChange={""}
+                      className="sr-only"
+                    />
                   </label>
-                </div>
-                <div className="text-sm">
-                  <a
-                    href=".forgot-password"
-                    className="font-medium text-blue-600 hover:text-blue-500"
-                  >
-                    Forgot your password?
-                  </a>
                 </div>
               </div>
               <div>
@@ -98,10 +137,10 @@ const Signup = () => {
                 </button>
               </div>
               <div className={`${styles.noramlFlex} w-full`}>
-                    <h4>회원이 아니십니까?</h4>
-                    <Link to="/sign-up" className="text-blue-600 pl-2">
-                        회원가입
-                    </Link>
+                <h4>회원이 아니십니까?</h4>
+                <Link to="/sign-up" className="text-blue-600 pl-2">
+                  회원가입
+                </Link>
               </div>
             </form>
           </div>
